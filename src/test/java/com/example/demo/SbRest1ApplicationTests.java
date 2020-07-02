fafbc95678f72393f23e637de7055207f30f343d
“srini4git” <vamsi4usa@gmail.com>
2020-07-02 17:04:18 -0400

    step1:Initial spring boot rest application

---
+++ src/test/java/com/example/demo/SbRest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
